0c203e93e4c47db0fca272a960414ea2a0b42927
Burningstone91 <dimitri.steiner.gl@gmail.com>
2024-03-04 12:00:56 +0100

    Add basic UI for the calculator

--- script.js
+++ script.js
@@ -4,23 +4,23 @@ let operator;
 
 
 function add(num1, num2) {
-    return num1 + num2;
+  return num1 + num2;
 }
 
 function subtract(num1, num2) {
-    return num1 - num2;
+  return num1 - num2;
 }
 
 function multiply(num1, num2) {
-    return num1 * num2;
+  return num1 * num2;
 }
 
 function divide(num1, num2) {
-    return num1 / num2;
+  return num1 / num2;
 }
 
 function operate(num1, num2, operator) {
-    return operator(num1, num2)
+  return operator(num1, num2)
 }
 
 num1 = 50;
