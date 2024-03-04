0f5dd5688a84ecc928350ab2e47dae77dc9e5dae
Burningstone91 <dimitri.steiner.gl@gmail.com>
2024-03-04 13:19:09 +0100

    Add logic to calculate result if an operator is pressed after two numbers and an operator have already been entered

--- script.js
+++ script.js
@@ -21,15 +21,21 @@ function divide(num1, num2) {
   return num1 / num2;
 }
 
+function remainder(num1, num2) {
+  return num1 % num2;
+}
+
 function operate(num1, num2, operator) {
   return operator(num1, num2)
 }
+
 
 buttons.forEach(button => {
   button.addEventListener("click", function handleClick(event) {
     const key = event.target;
     const keyValue = key.textContent;
     const keyType = key.className;
+
 
     if (keyType === "digit") {
       if (prevKeyType === null){
@@ -46,29 +52,34 @@ buttons.forEach(button => {
       
       display.value === "0" ? display.value = keyValue : display.value += keyValue;
       prevKeyType = "digit";
-
     }
 
-    if (keyType === "operator") {
-      switch (keyValue) {
-        case "%":
-          operator = remainder;
-          break;
-        case "÷":
-          operator = divide;
-          break;
-        case "+":
-          operator = add;
-          break;
-        case "-":
-          operator = subtract;
-          break;
-        case "x":
-          operator = multiply;
-          break;
-      }
 
-      prevKeyType = "operator";
+    if (keyType === "operator") {
+      if (num2 !== null) {
+        display.value = operate(Number(num1), Number(num2), operator);
+      } else {
+        switch (keyValue) {
+          case "%":
+            operator = remainder;
+            break;
+          case "÷":
+            operator = divide;
+            break;
+          case "+":
+            operator = add;
+            break;
+          case "-":
+            operator = subtract;
+            break;
+          case "x":
+            operator = multiply;
+            break;
+        }
+
+        display.value += keyValue;
+        prevKeyType = "operator";
+      }
     }
 
 
