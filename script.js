84d102bc2e08733624574dc1fdcbc44f4c41e645
Burningstone91 <dimitri.steiner.gl@gmail.com>
2024-03-04 11:20:12 +0100

    Add function to perform math operation based on given numbers and operator

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
+let num1 = 0;
+let num2 = 0;
+let operator;
+
+
 function add(num1, num2) {
     return num1 + num2;
 }
@@ -13,3 +18,15 @@ function multiply(num1, num2) {
 function divide(num1, num2) {
     return num1 / num2;
 }
+
+function operate(num1, num2, operator) {
+    return operator(num1, num2)
+}
+
+num1 = 50;
+num2 = 5;
+
+console.log(operate(num1, num2, add));
+console.log(operate(num1, num2, subtract));
+console.log(operate(num1, num2, multiply));
+console.log(operate(num1, num2, divide));
